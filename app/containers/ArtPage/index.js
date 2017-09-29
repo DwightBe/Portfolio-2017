8e7ff2d30366e4070fec5cac39bd3d5da7fa9284
Dwight Benignus <dwightbenignus@gmail.com>
2017-09-29 15:56:55 -0500

    media query

--- app/containers/ArtPage/index.js
+++ app/containers/ArtPage/index.js
@@ -13,6 +13,7 @@
 import React from 'react';
 import Gallery from 'react-grid-gallery';
 import { Images2017, Images2016, Images2011, TheyReturn } from './images';
+import MediaQuery from 'react-responsive';
 
 export default class ArtPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
@@ -31,30 +32,32 @@ export default class ArtPage extends React.PureComponent { // eslint-disable-lin
   render() {
     return (
       <div style={{ padding: '40px 0px' }}>
-        <nav style={{ width: '192px',
-                      zIndex: 3,
-                      position: 'fixed',
-                      top: '120px',
-                      lineHeight: '20px' }}>
-          <ul>
-            <li onClick={this.handleClick.bind(this, Images2017)}
-                style={{ height: '80px', fontSize: '18px', cursor: 'pointer' }}>
-              Guache/Watercolor Portraits, 2016 - 2017
-            </li>
-            <li style={{ height: '60px', fontSize: '18px', cursor: 'pointer' }}
-                onClick={this.handleClick.bind(this, Images2016)}>
-              Oil Portraits, 2016
-            </li>
-            <li style={{ height: '80px', fontSize: '18px', cursor: 'pointer' }}
-                onClick={this.handleClick.bind(this, Images2011)}>
-              Various Works, 2011 - 2015
-            </li>
-            <li style={{ height: '80px', fontSize: '18px', cursor: 'pointer' }}
-                onClick={this.handleClick.bind(this, TheyReturn)}>
-              They Return, comic, 2013
-            </li>
-          </ul>
-        </nav>
+        <MediaQuery minWidth={1000}>
+          <nav style={{ width: '192px',
+                        zIndex: 3,
+                        position: 'fixed',
+                        top: '120px',
+                        lineHeight: '20px' }}>
+            <ul>
+              <li onClick={this.handleClick.bind(this, Images2017)}
+                  style={{ height: '80px', fontSize: '18px', cursor: 'pointer' }}>
+                Guache/Watercolor Portraits, 2016 - 2017
+              </li>
+              <li style={{ height: '60px', fontSize: '18px', cursor: 'pointer' }}
+                  onClick={this.handleClick.bind(this, Images2016)}>
+                Oil Portraits, 2016
+              </li>
+              <li style={{ height: '80px', fontSize: '18px', cursor: 'pointer' }}
+                  onClick={this.handleClick.bind(this, Images2011)}>
+                Various Works, 2011 - 2015
+              </li>
+              <li style={{ height: '80px', fontSize: '18px', cursor: 'pointer' }}
+                  onClick={this.handleClick.bind(this, TheyReturn)}>
+                They Return, comic, 2013
+              </li>
+            </ul>
+          </nav>
+        </MediaQuery>
         <div style={{ maxWidth: '530px', margin: '0 auto' }}>
           <Gallery images={this.state.images}/>
         </div>
