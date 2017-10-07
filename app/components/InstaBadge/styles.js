d1471a641a0a4317c821475155baa3afc630fb89
Dwight Benignus <dwightbenignus@gmail.com>
2017-10-07 13:25:19 -0500

    instagram badge edit

--- app/components/InstaBadge/styles.js
+++ app/components/InstaBadge/styles.js
@@ -12,8 +12,9 @@ export const Badge = styled.div`
 	}
 
 	@media (max-width: 480px) {
-		font-size: 0.5em;
+		font-size: 0.4em;
 		top: 44px;
+		right: 25px;
 	}
 
 `;
@@ -27,7 +28,9 @@ export const Logo = styled.img`
 	@media (max-width: 767px) {
 		top: 1px;
 		left: -15px;
-
+	}
+	@media (max-width: 480px) {
+		top: 0px;
 	}
 
 `;
