c08715d0d04861b78e3482876e270282acc96481
Dwight Benignus <dwightbenignus@gmail.com>
2017-10-06 19:49:03 -0500

    stylings menu

--- app/components/SideNav/styles.js
+++ app/components/SideNav/styles.js
@@ -18,14 +18,14 @@ export const SubMenuItem = styled.li`
   font-size: 1em;
   cursor: pointer;
   padding: 20px 0px 20px;
-  @media (max-width: 767px) {
-    margin: 10px;
-    padding: 8px 0px 8px;
+  @media (max-width: 999px) {
+    margin: 15px;
+    padding: 6px 0px 6px;
     font-size: 1em;
   }
   @media (max-width: 480px) {
     font-size: 0.8em;
-    padding: 8px 0px 8px;
+    padding: 6px 0px 6px;
     margin: 10px;
   }
 `;
