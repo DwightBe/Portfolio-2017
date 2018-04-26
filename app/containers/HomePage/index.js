75f0e62deb90c386283b34c24ace5452f98c6d64
Dwight Benignus <dwightbenignus@gmail.com>
2018-04-26 12:03:09 -0500

    styling tweaks and secret links

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -31,7 +31,7 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
                   fontSize: '1.2em',
                   margin: '0 auto'}}>
             Hell<a href = "https://sudsclub.bandcamp.com/track/mexican-radio">o</a>! Welcome to my portfoli<a href="https://sudsclub.bandcamp.com/track/helpno">o</a>. I am an artist and software developer living and working in Austin, Tx. I have been showing
-            portraits in galleries since 2016, and working on software projects since 2012. This portfolio showcases all of my work in various media.
+            portraits in galleries since 2016, and working on software projects since 2012. This portfoli<a href="https://www.youtube.com/watch?v=wHvp4jgW0uc">o</a> showcases all of my work in various media.
           </div>
         </MediaQuery>
       </div>
