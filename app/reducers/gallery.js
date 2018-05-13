3f0b81ffc5716a924f30c462c3d4fde9b44993db
Dwight Benignus <dwightbenignus@gmail.com>
2018-05-13 14:20:40 -0500

    new art

--- app/reducers/gallery.js
+++ app/reducers/gallery.js
@@ -3,7 +3,7 @@ import {
 } from '../containers/App/constants'; // eslint-disable-line
 
 
-export default (state = 'Images2017', action) => {
+export default (state = 'Images2018', action) => {
   switch (action.type) {
     case 'SELECT_GALLERY':
       return action.gallery;
